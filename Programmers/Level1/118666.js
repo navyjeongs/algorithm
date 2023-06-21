b86d00528a924651d62803218243aff6bad12a31
navyjeongs <wjdtjr8649@naver.com>
2023-06-21 22:14:39 +0900

    Level1: 성격 유형 검사하기

---
+++ Programmers/Level1/118666.js
@@ -0,0 +1,17 @@
+function solution(survey, choices) {
+  const MBTI = {};
+  const types = ["RT", "CF", "JM", "AN"];
+
+  types.forEach((type) => type.split("").forEach((char) => (MBTI[char] = 0)));
+
+  console.log(MBTI);
+
+  choices.forEach((choice, index) => {
+    console.log(index);
+    const [disagree, agree] = survey[index];
+
+    MBTI[choice > 4 ? agree : disagree] += Math.abs(choice - 4);
+  });
+
+  return types.map(([a, b]) => (MBTI[b] > MBTI[a] ? b : a)).join("");
+}
