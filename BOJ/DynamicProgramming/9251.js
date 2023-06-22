d8f60bc19faef01b3dff3b8314ed10cd61aef70c
navyjeongs <wjdtjr8649@naver.com>
2023-06-22 22:20:59 +0900

    baekjoon: LCS

---
+++ BOJ/DynamicProgramming/9251.js
@@ -0,0 +1,18 @@
+const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const str1 = input[0];
+const str2 = input[1];
+
+let dp = Array.from(Array(str1.length + 1), () => Array(str2.length + 1).fill(0));
+
+for (let i = 1; i < str1.length + 1; i++) {
+  for (let j = 1; j < str2.length + 1; j++) {
+    if (str1[i - 1] === str2[j - 1]) {
+      dp[i][j] = dp[i - 1][j - 1] + 1;
+    } else {
+      dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+    }
+  }
+}
+
+console.log(dp[str1.length][str2.length]);
